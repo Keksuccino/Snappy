a503ddd35fea10014fc956528946dd22ee12437d
Keksuccino <Keksuccino@users.noreply.github.com>
2026-07-05 21:30:24 +0200

    ---

--- common/src/main/java/de/keksuccino/snappy/Snappy.java
+++ common/src/main/java/de/keksuccino/snappy/Snappy.java
@@ -11,7 +11,7 @@ public class Snappy {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "1.0.0";
     public static final String LOADER = Services.PLATFORM.getPlatformName().toUpperCase();
     public static final String MOD_ID = "snappy";
     public static final File MOD_DIR = createDirectory(new File(GameDirectoryUtils.getGameDirectory(), "/config/snappy"));
